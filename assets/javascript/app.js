56312dfecab49b117dc60cad49d47cc0a623b551
Oran Rush <oranrush@Orans-MBP.lan1>
2019-01-02 22:18:40 -0600

    Upload4

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,11 @@
 var panel = $('#quizarea');
-var countStartNumber = 30;
+var countStartNumber = 10;
 
 
 
 // Click events
 
-$(document).on('click', '#start-over', function(e) {
+$(document).on('click', '#restart', function(e) {
   game.reset();
 });
 
@@ -14,7 +14,7 @@ $(document).on('click', '.answer-button', function(e) {
 });
 
 $(document).on('click', '#start', function(e) {
-  $('#bkgrdwrapper').prepend('<h2>Time Remaining: <span id="counter-number">30</span> Seconds</h2>');
+  $('#bkgrdwrapper').prepend('<h2>Time Remaining: <span id="counter-number">10</span> Seconds</h2>');
   game.loadQuestion();
 });
 
@@ -97,6 +97,7 @@ var questions = [{
           game.timeUp();
         }
       },
+
       loadQuestion: function(){
         timer = setInterval(game.countdown, 1000);
         panel.html('<h2>' + questions[this.currentQuestion].question + '</h2>' );
@@ -104,26 +105,32 @@ var questions = [{
           panel.append('<button class="answer-button" id="button"' + 'data-name="' + questions[this.currentQuestion].answers[i] + '">' + questions[this.currentQuestion].answers[i]+ '</button>');
         }
       },
+
       nextQuestion: function(){
         game.counter = countStartNumber;
         $('#counter-number').html(game.counter);
         game.currentQuestion++;
         game.loadQuestion();
       },
+
       timeUp: function (){
         clearInterval(timer);
         $('#counter-number').html(game.counter);
     
         panel.html('<h2>Out of Time!</h2>');
-        panel.append('<h3>The Correct Answer was: ' + questions[this.currentQuestion].correctAnswer);
+        panel.append('<h3>The Correct Answer is: ' + questions[this.currentQuestion].correctAnswer);
         panel.append('<img src="' + questions[this.currentQuestion].image + '" />');
-    
+          console.log (game.currentQuestion)
+          console.log (questions.length);
+
+
         if (game.currentQuestion === questions.length - 1){
-          setTimeout(game.results, 3 * 1000);
+          setTimeout(game.results, 2 * 1000);
         } else {
-          setTimeout(game.nextQuestion, 3 * 1000);
+          setTimeout(game.nextQuestion, 2 * 1000);
         }
       },
+
       results: function() {
         clearInterval(timer);
     
@@ -132,30 +139,38 @@ var questions = [{
         panel.append('<h3>Correct Answers: ' + game.correct + '</h3>');
         panel.append('<h3>Incorrect Answers: ' + game.incorrect + '</h3>');
         panel.append('<h3>Unanswered: ' + (questions.length - (game.incorrect + game.correct)) + '</h3>');
-        panel.append('<br><button id="start-over">Start Over?</button>');
+        panel.append('<br><button id="restart">Restart</button>');
       },
+
       clicked: function(e) {
         clearInterval(timer);
     
+
         if ($(e.target).data("name") === questions[this.currentQuestion].correctAnswer){
           this.answeredCorrectly();
-        } else {
+        } 
+        
+        else {
           this.answeredIncorrectly();
         }
       },
+
       answeredIncorrectly: function() {
         game.incorrect++;
         clearInterval(timer);
-        panel.html('<h2>Nope!</h2>');
-        panel.append('<h3>The Correct Answer was: ' + questions[game.currentQuestion].correctAnswer + '</h3>');
+        panel.html('<h2>Incorrect!</h2>');
+        panel.append('<h3>The Correct Answer is: ' + questions[game.currentQuestion].correctAnswer + '</h3>');
         panel.append('<img src="' + questions[game.currentQuestion].image + '" />');
     
         if (game.currentQuestion === questions.length - 1){
-          setTimeout(game.results, 3 * 1000);
-        } else {
-          setTimeout(game.nextQuestion, 3 * 1000);
+          setTimeout(game.results, 2 * 1000);
+        } 
+        
+        else {
+          setTimeout(game.nextQuestion, 2 * 1000);
         }
       },
+
       answeredCorrectly: function(){
         clearInterval(timer);
         game.correct++;
@@ -163,11 +178,14 @@ var questions = [{
         panel.append('<img src="' + questions[game.currentQuestion].image + '" />');
     
         if (game.currentQuestion === questions.length - 1){
-          setTimeout(game.results, 3 * 1000);
-        } else {
-          setTimeout(game.nextQuestion, 3 * 1000);
+          setTimeout(game.results, 2 * 1000);
+        } 
+        
+        else {
+          setTimeout(game.nextQuestion, 2 * 1000);
         }
       },
+
       reset: function(){
         this.currentQuestion = 0;
         this.counter = countStartNumber;
